efdda0072f52db8aeb5679160277af76168fd56a
Diego <diegoestrada35@gmail.com>
2022-07-17 19:51:58 -0300

    Mas clases services

--- src/main/java/com/andinos/hca/model/service/IVentaService.java
+++ src/main/java/com/andinos/hca/model/service/IVentaService.java
@@ -1,5 +1,15 @@
 package com.andinos.hca.model.service;
 
-public interface IVentaService {
+import com.andinos.hca.model.entity.Venta;
 
+import java.util.List;
+
+public interface IVentaService {
+    public List<Venta> findAll();
+
+    public void save(Venta venta);
+
+    public Venta findOne(Long id);
+
+    public void delete(Long id);
 }
---
+++ src/main/java/com/andinos/hca/model/service/VentaServiceImpl.java
@@ -0,0 +1,38 @@
+package com.andinos.hca.model.service;
+
+import com.andinos.hca.model.dao.IVentaDAO;
+import com.andinos.hca.model.entity.Venta;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class VentaServiceImpl implements IVentaService{
+
+    private IVentaDAO ventaDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Venta> findAll() {
+        return (List<Venta>) ventaDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void save(Venta venta) {
+        ventaDao.save(venta);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Venta findOne(Long id) {
+        return ventaDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        ventaDao.deleteById(id);
+    }
+}
